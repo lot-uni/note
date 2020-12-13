fabe738a25ab8ac1c2e90d6bb45274c826a01787
lot-uni <hikaru.unity.1110@gmail.com>
2020-12-14 07:58:01 +0900

    Initial Commit

---
+++ note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  note
+//
+//  Created by lot-uni on 2020/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
